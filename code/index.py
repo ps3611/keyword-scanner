811398b117474361e52db0404847c661d589e516
Peter Schindler <petertasschindler@gmail.com>
2018-08-31 22:42:13 +0200

    nice index script

--- code/index.py
+++ code/index.py
@@ -1,265 +1,44 @@
-urls = [
-
-'http://www.greenlandseafood.eu',
-'http://www.dcoop.es',
-'http://www.riberebro.com/nuestro-grupo/conservas-vegetales',
-'http://www.faiberti.com',
-'http://www.houssydrink.com/',
-'http://www.jomartobacco.com ',
-'http://www.agrosevilla.com',
-'http://www.centrale-ethnique.com',
-'http://www.martinavarro.es',
-'http://www.deutschesee.de',
-'http://subcarnechevarria.com/',
-'http://www.cuizsqfood.com',
-'http://www.gruposos.com',
-'http://breizhprimeurs.com',
-'http://www.ccelra.com',
-'http://www.congeladossariego.com',
-'http://www.patel.es',
-'http://www.nicefruit.com',
-'http://www.acamacho.com',
-'http://www.faccsa.es',
-'http://www.aromafreshprof.com',
-'http://www.cargill.com/',
-'http://ww2.carob.es/',
-'http://www.fcostabrava.com',
-'http://www.borges-bain.com',
-'http://www.friusa.es/',
-'http://compagniedespeches.com/index.htm',
-'http://www.reunimer.com/',
-'http://eurial.eu/fr/eurial/chiffres-cles-0',
-'http://www.selimex.com',
-'http://www.fesa.co.uk',
-'http://www.godeland.landgard.de',
-'http://www.volmary.com',
-'http://www.bavaria-saat.de',
-'http://www.vitacress.com',
-'http://www.montronill.com/en/',
-'http://www.bdfingredients.com',
-'http://www.joceane.fr/fr-accueil.html',
-'http://www.ccdp.it',
-'http://www.oceane.tm.fr',
-'http://www.sirventalmendras.com',
-'http://www.dipisasrl.com/en/fruit-and-vegetables-wholesale-bologna/',
-'http://www.melinda.it',
-'http://www.agrial.com',
-'http://www.alpha-bay.com',
-'http://www.naogafoods.com',
-'http://www.paprifood.com/',
-'http://www.morenot.es',
-'http://www.chocoweb.com',
-'http://www.lagnascogroup.it/',
-'http://www.frulexxo.com',
-'http://www.asprofrut.com/welcome.asp?pag=2',
-'http://www.grupofuertes.com/en/',
-'http://www.incarlopsa.es',
-'http://www.fratelliberetta.com/en/home',
-'http://www.beckettsfoods.co.uk',
-'http://www.deraza.es',
-'http://www.sanchezalcaraz.com',
-'http://www.lambweston.eu/',
-'http://www.axereal.com/',
-'http://www.vallcompanys.es',
-'http://www.gadus.be/index-nl.aspx',
-'http://www.viciunaigroup.eu/en',
-'http://www.uniqueseafood.co.uk',
-'http://www.ideal-foods.co.uk',
-'http://www.magellan-import.com/index.php?entry=0&mmond=0&lang=0',
-'http://www.capespan.com',
-'http://www.jaimellorca.com',
-'http://www.grupbaucells.com',
-'http://www.metarex.it',
-'http://www.bonesandmeats.com',
-'http://www.groupelegraet.fr/fr/le-groupe.aspx',
-'http://www.ldc.fr',
-'http://www.gruppomartelli.com',
-'http://www.citterio.com',
-'http://www.sva-jeanroze.com',
-'http://www.suincom.it',
-'http://www.martinialimentare.it',
-'http://www.fileni.it',
-'http://www.alcaruno.it',
-'http://www.inalca.it',
-'http://www.sicilianispa.com',
-'http://www.sprehe.de',
-'http://www.somerdale.co.uk',
-'http://www.nuovalaselvapesca.it',
-'http://www.dituri.it',
-'http://www.foodsimport.com',
-'http://www.comavicola.com',
-'http://www.sapmer.com/',
-'http://www.amcgrupo.eu',
-'http://www.sprehe.de',
-'http://www.terrena.fr/',
-'http://www.selonda.com/',
-'http://www.royalgreenland.com/from-sea-to-table/',
-'http://andersonpelagic.com/',
-'http://www.gebls.es',
-'https://franzuela.com/',
-'http://www.pescapuerta.es',
-'http://www.alvisa.es',
-'https://ultimocateringandevents.com.au/',
-'http://www.americaimportsl.com',
-'http://www.angussoftfruits.co.uk',
-'http://www.vidafresh.co.uk',
-'http://www.procsea.com',
-'http://www.acesur.com',
-'http://www.legumes-primeurs-bretons.fr',
-'http://www.casi.es/'
-
-]
-
-key_words = {
-
-    'fish':5,
-    'fresh':5,
-    'frozen':5,
-    'meat products':5,
-    'milk':5,
-    'cheese':5,
-    'vegetables':5,
-    'fresh meat':5,
-    'fisch':5,
-    'gefroren':5,
-    'fleischprodukte':5,
-    'fleisch':5,
-    'milch':5,
-    'käse':5,
-    'gemüse':5,
-    'frischfleisch':5,
-    'pescado':5,
-    'fresco':5,
-    'congelado':5,
-    'productos de carne':5,
-    'carne':5,
-    'leche':5,
-    'queso':5,
-    'vegetales':5,
-    'carne fresca':5,
-    'poisson':5,
-    'frais':5,
-    'congelé':5,
-    'produits de la mer':5,
-    'viande':5,
-    'lait':5,
-    'fromage':5,
-    'légumes':5,
-    'viande fraîche':5,
-    'Europe':3,
-    'international':3,
-    'countries':3,
-    'continents':3,
-    'distributing':3,
-    'distribution':3,
-    'America':3,
-    'Africa':3,
-    'Asia- Pacific':3,
-    'worldwide':3,
-    'world-wide':3,
-    'China':3,
-    'South America':3,
-    'World':3,
-    'export':3,
-    'exporting':3,
-    'exporter':3,
-    'import':3,
-    'importer':3,
-    'importing':3,
-    'international markets':3,
-    'global':3,
-    'Europa':3,
-    'Länder':3,
-    'Kontinente':3,
-    'verteilen':3,
-    'Verteilung':3,
-    'Amerika':3,
-    'Afrika':3,
-    'Asien-Pazifik':3,
-    'Weltweit':3,
-    'Südamerika':3,
-    'exportieren':3,
-    'exporteur':3,
-    'importeur':3,
-    'importieren':3,
-    'internationale Märkte':3,
-    'internacional':3,
-    'países':3,
-    'continentes':3,
-    'distribuimos':3,
-    'distribución':3,
-    'Asia-Pacífico':3,
-    'mundial':3,
-    'en todo el mundo':3,
-    'America del Sur':3,
-    'mundo':3,
-    'exportación':3,
-    'exportador':3,
-    'mercados internacionales':3,
-    'transporter':3,
-    'pays':3,
-    'continents':3,
-    'distribuer':3,
-    'Amérique':3,
-    'Afrique':3,
-    'Asie-Pacifique':3,
-    'mondial':3,
-    'Chine':3,
-    'Amérique du sud':3,
-    'monde':3,
-    'exportation':3,
-    'exportateur':3,
-    'importateur':3,
-    'Marchés internationaux':3,
-    'Globale':3,
-    'Welt':3,
-    'English':1,
-    'Deutsch':1,
-    'Francais':1,
-    'Polski':1,
-    'Italiano':1,
-    'Español':1,
-    'French':1,
-    'German':1,
-    'Italian':1,
-    'Spanish':1
-
-}
-
 from urllib.request import urlopen, Request
+import pandas as pd
 from bs4 import BeautifulSoup
 import os
+import datetime
 import ssl
+print('Starting keyword scraper script...')
 
+urls_df = pd.read_excel('../input/input_websites.xlsx')
+keywords_df = pd.read_excel('../input/model.xlsx')
+output_df = {'website':[],'score':[],'key_words':[]}
 
-#os.chdir("/Users/adchata")
-output = open("out3.csv", "w")
+n_urls = len(urls_df['urls'])
+# n_urls = 5
 
-
-
-for url in urls:
+for url in urls_df['urls'][0:n_urls]:
     try:
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         openedUrl = urlopen(req)
-
         soup = BeautifulSoup(openedUrl, 'html.parser')
         soup = soup.encode('ascii').decode("ISO-8859-1")
-        has_key_words = []
+
+        key_words = []
         score = 0
-        for word,weight in key_words.items():
+        for i in range(keywords_df.shape[0]):
+            word = keywords_df.iloc[i]['key_words']
+            weight = keywords_df.iloc[i]['weight']
             if word in soup:
-                has_key_words.append(word)
+                key_words.append(word)
                 score += weight
 
-        # For debugging purposes
-        # print("(" + str(score) + ") " + url + ' ---> ' + str(has_key_words))
+        output_df['website'].append(url)
+        output_df['score'].append(score)
+        output_df['key_words'].append(key_words)
+        print(url + ' ===> ' + str(score))
 
-        print("(" + str(score) + ") " + url)
-
-        output.write(str(score))
-        output.write(",")
-        output.write(url)
-        output.write("\n")
     except:
         print('----',url,'failed... going onto the next url!')
-output.close()
+
+output_df = pd.DataFrame(output_df).sort_values(by='score',ascending=False)
+output_df = output_df[['website','score','key_words']]
+output_path = '../output/output_'+str(datetime.datetime.now())+'.csv'
+output_df.to_csv(output_path, sep=',')
+print('Output saved as... ' + output_path + ' in output/ folder!')
